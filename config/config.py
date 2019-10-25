4ae9ae9fef6a08db6a300ad21959913c58d2529e
Irina <irusena@gmail.com>
2019-10-25 16:04:49 +0300

    no message

--- config/config.py
+++ config/config.py
@@ -1,2 +1,2 @@
-Device_address = 0x1e  # HMC5883L magnetometer device address
-Declination = -0.00669 # define declination angle of location where measurement going to be done
+DEVICE_ADDRESS = 0x1e  # HMC5883L magnetometer device address
+DECLINATION = -0.00669 # define declination angle of location where measurement going to be done
